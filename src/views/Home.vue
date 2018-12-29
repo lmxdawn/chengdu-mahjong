1df12b0ebb2ff179e5bd1dbba238555debc95ef4
lmxdawn@qq.com <lmxdawn@qq.com>
2018-12-29 14:27:58 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="view-container">
+        <!--顶部玩家-->
+        <div class="user-top"></div>
+        <!--左手边玩家-->
+        <div class="user-left"></div>
+        <!--底部玩家-->
+        <div class="user-bottom">
+
+
+
+        </div>
+        <!--右手边玩家-->
+        <div class="user-right"></div>
 
     </div>
 </template>
@@ -17,8 +29,17 @@ export default {
 
 <style type="text/scss" lang="scss">
 .view-container {
+    position: relative;
     width: 750px;
     height: 400px;
     background-color: #066e6d;
+}
+.user-bottom {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 160px;
+    background-color: red;
 }
 </style>
